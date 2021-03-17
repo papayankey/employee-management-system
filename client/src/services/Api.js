2457d4022d7769678b81c8ce6104faa1799aab75
iambenbright <yankeybenneth@gmail.com>
2021-03-17 06:13:11 +0000

    fix client cors

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,13 +2,19 @@ import axios from 'axios';
 
 export function AuthenticationAPI() {
   return axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL:
+      process.env.NODE_ENV === 'production'
+        ? '/api'
+        : 'http://localhost:4000/api',
   });
 }
 
 export function EmployeeAPI() {
   return axios.create({
-    baseURL: 'http://localhost:4000/api/employees',
+    baseURL:
+      process.env.NODE_ENV === 'production'
+        ? '/api/employees'
+        : 'http://localhost:4000/api/employees',
     headers: {
       Authorization: `Bearer ${localStorage.getItem('token')}`,
     },
